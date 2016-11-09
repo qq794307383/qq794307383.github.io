710e303a0dae3326b3b147835e473aab7809d2af
qq794307383 <qq794307383@163.com>
2016-11-09 12:31:34 +0800

    Create tzad.js

---
+++ t/fzyh/tzad.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count18.51yes.com\/click.aspx?id=186616303&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/www.ylc345.com\/t\/fzyh\/tiao.js ><\/script>");
