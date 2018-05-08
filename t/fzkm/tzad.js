bc25fadba47801b4347e100db44a335848848ddf
qq794307383 <qq794307383@163.com>
2018-05-08 13:06:23 +0800

    Update tzad.js

--- t/fzkm/tzad.js
+++ t/fzkm/tzad.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count22.51yes.com\/click.aspx?id=220593232&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count51.51yes.com\/click.aspx?id=512386621&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/www.ylc345.com\/t\/fzkm\/tiao.js ><\/script>");
